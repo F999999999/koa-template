2412303a9aaee5a4fa468df216cc5c3e10c0cd33
F999999999 <9811@88.com>
2022-08-22 12:35:04 +0800

    新增接口示例：添加mysql+jwt+joi+dotenv

--- app.js
+++ app.js
@@ -1,42 +1,81 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
+const Koa = require("koa");
+const app = new Koa();
+const views = require("koa-views");
+const json = require("koa-json");
+const onerror = require("koa-onerror");
+const bodyparser = require("koa-bodyparser");
+const logger = require("koa-logger");
+const jwt = require("koa-jwt");
+const jsonwebtoken = require("jsonwebtoken");
+// 配置 process.env
+require("dotenv").config();
 
-const index = require('./routes/index')
+// 导入路由
+const index = require("./routes/index");
+const users = require("./routes/users");
 
-// error handler
-onerror(app)
+// 错误处理
+onerror(app);
 
-// middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+// 中间件
+// 使用 koa-jwt 中间件 未拦截客户端在调用接口时 如果请求头中没有设置 token 则返回 401
+app.use((ctx, next) => {
+  return next().catch((err) => {
+    if (err.status === 401) {
+      ctx.status = 401;
+      ctx.body = {
+        status: 401,
+        message: "没有访问权限",
+      };
+    } else {
+      throw err;
+    }
+  });
+});
+// 解析 token 数据
+app.use((ctx, next) => {
+  ctx.request.docodeToken = jsonwebtoken.decode(
+    ctx.request.headers?.authorization?.slice(7)
+  );
+  return next();
+});
+// 设置哪些接口不需要 token
+app.use(
+  jwt({ secret: process.env.JWT_SECRET_KEY }).unless({
+    path: [/^\/public/, /^\/users\/register/, /^\/users\/login/],
+  })
+);
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"],
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(require("koa-static")(__dirname + "/public"));
+
+app.use(
+  views(__dirname + "/views", {
+    extension: "pug",
+  })
+);
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
-
-// routes
-app.use(index.routes()).use( index.allowedMethods())
-
-// error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
-module.exports = app
+// routes
+app.use(index.routes()).use(index.allowedMethods());
+app.use(users.routes()).use(users.allowedMethods());
+
+// error-handling
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
+});
+
+module.exports = app;
