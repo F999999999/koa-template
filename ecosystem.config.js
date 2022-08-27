2dda88e781a36e71794e427e830be23602721ee4
F999999999 <9811@88.com>
2022-08-27 10:59:48 +0800

    配置actions自动部署工作流

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      //pm2名字
+      name: "koa2_serve",
+      //pm2运行脚本
+      script: "bin/www",
+      //环境变量
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+  deploy: {
+    // "production" 环境名称
+    production: {
+      // post-deploy action
+      "post-deploy": "pm2 reload ecosystem.config.js --env production",
+      // "post-deploy": "npm install && pm2 reload ecosystem.config.js --env production",
+    },
+  },
+};
